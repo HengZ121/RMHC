6c3357b6ad296dc6a132090b5091dc87e1d32e9d
Heng_Zhang <hzhan274@uOttawa.ca>
2023-05-28 00:46:38 -0400

    introduced int

--- data.py
+++ data.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from scipy.fft import fft, ifft
+from statsmodels.tsa.stattools import acf
 from sklearn.preprocessing import StandardScaler
 
 # Location of fMRI data
@@ -26,7 +27,7 @@ class Dataset():
         self.area = area
 
         # Helper Attributes
-        self.psd  = []
+        self.acw  = []
         self.ids  = []
         self.run  = []
 
@@ -43,23 +44,31 @@ class Dataset():
             if (task in filename) and (area in filename):
                 df = pd.read_csv(os.path.join(fmri_path, filename), sep=",", header=None)
                 fs = 0.9
-                psd_df = []
-                
-                # Transfer voxels' activation values by time to by frequency (Power Spectral Density)
-                for index, row in df.iterrows():
-                    (f, S) = scipy.signal.periodogram(np.array(row), fs, scaling='density')
-                    psd_df.append(S)
-                
-                # Singal Strength Value -> Change in Singal Strength Value
-                for S in psd_df:
-                    ave_signal_strength = np.mean(S)
-                    for s in S:
-                        s = (s - ave_signal_strength) if s > ave_signal_strength else (ave_signal_strength - s)
-                
-                psd_df = np.array(psd_df)
 
-                # Calculate the average singal strength of all voxel at a time point
-                psd_mean = np.mean(psd_df, axis=0)
+                acw_df = []
+                for index, row in df.iterrows():
+                    acw5, acw0, acffunc = acw(row)
+                    acw_df.append([acw5, acw0])
+                acw_mean = np.mean(acw_df, axis=0)
+                self.acw.append(acw_mean)
+
+                # psd_df = []
+                # # Transfer voxels' activation values by time to by frequency (Power Spectral Density)
+                # for index, row in df.iterrows():
+                #     (f, S) = scipy.signal.periodogram(np.array(row), fs, scaling='density')
+                #     psd_df.append(S)
+                
+                # # Singal Strength Value -> Change in Singal Strength Value
+                # for S in psd_df:
+                #     ave_signal_strength = np.mean(S)
+                #     for s in S:
+                #         s = (s - ave_signal_strength) if s > ave_signal_strength else (ave_signal_strength - s)
+                
+                # psd_df = np.array(psd_df)
+
+                # # Calculate the average singal strength of all voxel at a time point
+                # psd_mean = np.mean(psd_df, axis=0)
+                # self.psd.append(psd_mean)
 
                 # N = len(df.columns)
                 # fft_df = []
@@ -72,14 +81,13 @@ class Dataset():
                 #     fft_df.append(fft_row[:N//2])
 
 
-                # # Visualize PSD and save image
-                # plt.imshow(np.array(psd_df, dtype=float), cmap='Greys', vmin = 0, vmax= 7000, aspect='auto')
+                # Visualize ACW and save image
+                # plt.imshow(np.array(acw_df, dtype=float), cmap='Greys', aspect='auto')
                 # plt.xlabel("Frequency")
                 # plt.ylabel("Voxels")
                 # plt.savefig(os.path.join(r'C:\Users\Heng\Desktop\spectrograms', filename.replace('.1D','')+'.png'), bbox_inches='tight')
                 # plt.close("all")
-
-                self.psd.append(psd_mean)
+                
                 self.ids.append(filename.split('_')[2])
                 self.run.append('1' if '1' in filename.split('_')[1] else '2')
 
@@ -134,24 +142,20 @@ class Dataset():
         for index in fmri_to_be_deleted:
                 del self.ids[index]
                 del self.run[index]
-                del self.psd[index]
-        x = []
-        mean_of_participants = np.mean(self.psd, axis=0)
-        for p in self.psd:
-            self.labels.append(0 if np.sum(p - mean_of_participants)< 0 else 1)
-            y = p - mean_of_participants
-            x = np.array([i for i in range(len(y))])
-            plt.scatter(x, y, color="red")
-            plt.show()
+                del self.acw[index]
+
+        mean_of_participants = np.mean(self.acw, axis=0)
+        for p in self.acw:
+            self.labels.append(p)
             # x.append(np.sum(p - mean_of_participants))
         print("Labels Loaded")
-        # # Show distribution of labels
-        # x = np.sort(np.array(x))
-        # y = np.array([i for i in range(len(x))])
-        # print("Labels Loaded")
-        # plt.title(self.area)
-        # plt.scatter(y, x, color="red")
-        # plt.show()
+        # Show distribution of labels
+        x = np.sort(np.array([acw[1] for acw in self.acw]))
+        y = np.array([i for i in range(len(x))])
+        print("Labels Loaded")
+        plt.title(self.area)
+        plt.scatter(y, x, color="red")
+        plt.show()
         scaler.fit(self.features)
         self.features = scaler.transform(self.features)
 
@@ -169,7 +173,7 @@ class Dataset():
     
 #% ACW
 def acw(ts, n_lag=None, fast=True):
-    acffunc = stattools.acf(ts, nlags=n_lag, qstat=False, alpha=None, fft=fast)
+    acffunc = acf(ts, nlags=n_lag, qstat=False, alpha=None, fft=fast)
     acw5 = (2 * np.argmax(acffunc <= 0.5) - 1)
     acw0 = (2 * np.argmax(acffunc <= 0) - 1)
     return acw5, acw0, acffunc
